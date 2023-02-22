9097aa59ccd296395653f2a26d8b2aa278ef2714
Dennis K <dennisagbokpe@gmail.com>
2023-02-22 16:02:26 -0600

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,20 @@
+import bugger from '../assets/404/bugger.png';
+import oops from "../assets/404/404.png";
+import {RiHomeGearFill} from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 const NotFound = () => {
     return ( 
-        <div className="text-center">
-            <h1>Not found</h1>
+        <div className="text-center flex items-center justify-center pt-20 mt-10 flex-col ">
+            <img src={oops} alt='Not Found' className='sm:h-96 h-64 '/>
+            
+            <h1 className='font-bold text-2xl mt-2'>Page Not Found!</h1>
+            <p className='text-gray-400'>Please Go Back Home</p>
+            <Link to='/'>
+            <RiHomeGearFill className='text-2xl text-teal-500 cursor-pointer hover:shadow-sm 
+            duration-150 transition
+            '/>
+            </Link>
+            
         </div>
      );
 }
